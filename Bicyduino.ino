d1ae926637fd3f5e8631adfa287b52cfb337fd71
braoutch <antoinelassagne@gmail.com>
2015-06-03 15:51:29 +0200

    Add accelerometer
    Initialization, measurement and display in serialport.
    Next step : debug then LCD and some real testing!

--- Bicyduino.ino
+++ Bicyduino.ino
@@ -3,9 +3,16 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9340.h"
 
-/////////////////
-/////DISPLAY/////
-/////////////////
+///////////////////////////////////
+//////////////Accelerometer////////
+#include <Wire.h>
+const int MPU=0x68;  // I2C address of the MPU-6050
+int16_t Tmp,GyX,GyY,GyZ; //Accelerometer datas
+///////////END ACCELEROMETER///////
+///////////////////////////////////
+
+//////////////////////////
+//////////DISPLAY/////////
 #if defined(__SAM3X8E__)
     #undef __FlashStringHelper::F(string_literal)
     #define F(string_literal) string_literal
@@ -21,6 +28,7 @@
 
 Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _rst);
 /////////////END DISPLAY///////////
+////////////////////////////////////
 
 unsigned long distance;
 float currentSpeed,meanSpeed;
@@ -39,6 +47,14 @@ void setup()
         Serial.begin(9600);
         tft.begin(); //Screen initialization
 	//attachInterrupt(0,UpdateSpeed,RISING);
+
+	/////////ACCELEROMETER////////
+	Wire.begin();
+	Wire.beginTransmission(MPU);
+  	Wire.write(0x6B);  // PWR_MGMT_1 register
+  	Wire.write(0);     // set to zero (wakes up the MPU-6050)
+	Wire.endTransmission(true);
+	/////////////////////////////
 }
 
 
@@ -70,17 +86,28 @@ void Measurement()
 	int raw = analogRead(A0);   // Range : 0..1024
         if(raw > 600 && detected ==false)
         {
-          detected = true;
-          UpdateSpeed();
-          UpdateMeanSpeed();
-          Display(raw);
+        	detected = true;
+        	UpdateSpeed();
+        	UpdateMeanSpeed();
+        	Display(raw);
+        	
+        	///ACCELEROMETER///
+        	Wire.beginTransmission(MPU);
+		Wire.write(0x41);  // starting with register 0x41 (ACCEL_XOUT_H)
+  		Wire.endTransmission(false);
+  		Wire.requestFrom(MPU,8,true);  // request a total of 2*4 registers
+  		Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  		GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  		GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  		GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
         }
+        
         else if (raw < 600)
         detected = false;
 }
 
-void UpdateSpeed(){ ////CALL BY INTERRUPTION
-
+void UpdateSpeed()
+{ ////CALL BY INTERRUPTION
 	lastDuration = millis() - lastRising;
 
 	lastRising = millis();
@@ -88,13 +115,10 @@ void UpdateSpeed(){ ////CALL BY INTERRUPTION
 	currentSpeed = (float)diameter / (float)lastDuration ;
 
 	distance = distance+diameter;
-
 }
 
 
-
 void UpdateMeanSpeed()
-
 {
 
 	meanSpeed = (float)distance/((float)millis()-(float)timeStoppped);
@@ -117,6 +141,12 @@ void Display(int raw)
 
         Serial.print("Output: ");
 	Serial.println(raw);
-        
+	
+	Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  	Serial.print(" | GyX = "); Serial.print(GyX);
+  	Serial.print(" | GyY = "); Serial.print(GyY);
+  	Serial.print(" | GyZ = "); Serial.println(GyZ);
+             
+        Serial.print("Awaken Time: ");
         Serial.println(millis());
 }
